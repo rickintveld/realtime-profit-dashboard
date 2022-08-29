963799e62d6dc9ac93103f268e0c726f72ad3379
Rick in 't veld <rick.in.t.veld@opinity.nl>
2022-08-29 15:49:22 +0200

    Updated the add fake profit js

--- profit.js
+++ profit.js
@@ -11,13 +11,3 @@ socket.emit("add_profit", {
   commission: 15,
   profit: 1000,
 });
-
-// 19-09 14:00
-// 1. paspoort
-// 2. toestemming van inwoning
-// 3. copy van ouders
-// 4. copy koop contract
-// 5. formulier adres wijziging
-// 6. formulier hervestiging
-
-// 722868
